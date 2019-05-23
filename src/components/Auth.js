911891fb98c8754949c5d587164db07a83efd0f7
Bru <annabru.abh@gmail.com>
2019-05-23 09:46:13 +0200

    Added Auth.scss - started styling login form

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React from "react";
 import { Consumer } from "./Context";
-
+import "../styles/Auth.scss";
 
 export default function Auth() {
   return (
     <Consumer>
       {value => {
-        const { handleChange, handleLogin, handleRegister, user, password } = value;
-        return (<form>
-          <label>
-            Username:
-              <input type="text" name="name" value={user} required onChange={(e) => handleChange('name', e)} />
-          </label>
-          <label>
-            Password:
-    <input type="password" name="password" value={password} required onChange={(e) => handleChange('password', e)} />
-          </label>
-          <input type="submit" value="Login" onClick={handleLogin} />
-          <input type="submit" value="Register" onClick={handleRegister} />
-        </form>)
-      }
-      }
+        const {
+          handleChange,
+          handleLogin,
+          handleRegister,
+          user,
+          password
+        } = value;
+        return (
+          <div className="auth-container">
+            <form className="auth-form">
+              <label>
+                Username:
+                <input
+                  type="text"
+                  name="name"
+                  value={user}
+                  placeholder="Username"
+                  required
+                  onChange={e => handleChange("name", e)}
+                />
+              </label>
+              <label>
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  placeholder="Password"
+                  required
+                  onChange={e => handleChange("password", e)}
+                />
+              </label>
+              <input type="submit" value="Login" onClick={handleLogin} />
+              <input type="submit" value="Register" onClick={handleRegister} />
+            </form>
+          </div>
+        );
+      }}
     </Consumer>
-
-  )
+  );
 }
