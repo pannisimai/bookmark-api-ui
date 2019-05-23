f310cfce5b2694bca4dfe2fdbbc3eb757c91544c
Bru <annabru.abh@gmail.com>
2019-05-23 11:33:54 +0200

    Styling bookmarklist and bookmarkdisplay - hover effects, headers, CLASSY

--- src/components/BookmarkItem.js
+++ src/components/BookmarkItem.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Consumer } from "./Context"
+import React, { Component } from "react";
+import { Consumer } from "./Context";
 import "../styles/BookmarkItem.scss";
 
 export default class BookmarkItem extends Component {
@@ -7,8 +7,13 @@ export default class BookmarkItem extends Component {
     const { showdetailsFunc, bookmark } = this.props;
     return (
       <React.Fragment>
-        <li onClick={() => showdetailsFunc(bookmark.title)}>{bookmark.title}</li>
+        <li
+          className="bookmark-single-item"
+          onClick={() => showdetailsFunc(bookmark.title)}
+        >
+          {bookmark.title}
+        </li>
       </React.Fragment>
-    )
+    );
   }
 }
--- src/components/BookmarkList.js
+++ src/components/BookmarkList.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import BookmarkItem from "./BookmarkItem"
+import React, { Component } from "react";
+import BookmarkItem from "./BookmarkItem";
 import { Consumer } from "./Context";
 import "../styles/BookmarkList.scss";
 
@@ -9,20 +9,24 @@ export default class BookmarkList extends Component {
       <Consumer>
         {({ showdetailsFunc, bookmarks }) => {
           return (
-            <div className="col-4 bookmarklist">
+            <div className="col-4 bookmark-list container">
+              <h5>Your bookmarks:</h5>
               <ul>
-                {bookmarks.map((bookmark) => {
-                  console.log(bookmark)
-                  return <BookmarkItem
-                    bookmark={bookmark}
-                    showdetailsFunc={showdetailsFunc}
-                    key={bookmark._id} />
+                {bookmarks.map(bookmark => {
+                  console.log(bookmark);
+                  return (
+                    <BookmarkItem
+                      bookmark={bookmark}
+                      showdetailsFunc={showdetailsFunc}
+                      key={bookmark._id}
+                    />
+                  );
                 })}
               </ul>
             </div>
-          )
+          );
         }}
       </Consumer>
-    )
+    );
   }
 }
