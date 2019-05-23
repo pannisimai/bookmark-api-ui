158e61a742593622e8c2cf4be897cd986664dccf
kgrhartlage <webdevkonto@gmail.com>
2019-05-23 10:47:56 +0200

    Add delete functionality

--- src/components/BookmarkDisplay.js
+++ src/components/BookmarkDisplay.js
@@ -7,14 +7,17 @@ export default class BookmarkDisplay extends Component {
       <div className="col-8 bookmarkdisplay">
         <div className="country-display-container">
           <Consumer>
-            {({ selectedBookmark }) => {
+            {({ selectedBookmark, deleteFunc }) => {
               if (selectedBookmark) {
                 return (< div className="country-disp-text">
                   <p>{selectedBookmark.title}</p>
                   <p>{selectedBookmark.shortDescription}</p>
                   <a href={selectedBookmark.url} target="_blank" rel="noopener noreferrer"> {selectedBookmark.url}</a>
-                  <button type="button" className="btn btn-danger">Delete</button>
-                  <button type="button" classname="btn btn-info">Edit</button>
+                  <button
+                    onClick={() => deleteFunc(selectedBookmark._id)}
+                    type="button"
+                    className="btn btn-danger">Delete</button>
+                  <button type="button" className="btn btn-info">Edit</button>
                 </div>)
               }
             }}
