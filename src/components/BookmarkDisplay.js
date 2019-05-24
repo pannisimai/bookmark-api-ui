3dcf092368a3985f63296d013e3f68a4b4b84eca
kgrhartlage <webdevkonto@gmail.com>
2019-05-24 18:06:36 +0200

    Fix merge

--- src/components/BookmarkDisplay.js
+++ src/components/BookmarkDisplay.js
@@ -1,30 +1,59 @@
 import React, { Component } from 'react';
-import { Consumer } from "./Context"
+import { Consumer } from './Context';
+import '../styles/BookmarkDisplay.scss';
 
 export default class BookmarkDisplay extends Component {
   render() {
     return (
-      <div className="col-8 bookmarkdisplay">
-        <div className="country-display-container">
-          <Consumer>
-            {({ selectedBookmark, deleteFunc }) => {
-              if (selectedBookmark) {
-                return (< div className="country-disp-text">
-                  <p>{selectedBookmark.title}</p>
-                  <p>{selectedBookmark.shortDescription}</p>
-                  <a href={selectedBookmark.url} target="_blank" rel="noopener noreferrer"> {selectedBookmark.url}</a>
-                  <button
-                    onClick={() => deleteFunc(selectedBookmark._id)}
-                    type="button"
-                    className="btn btn-danger">Delete</button>
-                  <button type="button" className="btn btn-info">Edit</button>
-                </div>)
-              }
-            }}
-          </Consumer>
-
-        </div>
-      </div >
-    )
+      <div className='col-8 bookmark-display'>
+        <Consumer>
+          {({ selectedBookmark, deleteFunc }) => {
+            if (selectedBookmark) {
+              return (
+                <div className='bookmark-display-container'>
+                  <div className='bookmark-display-text'>
+                    {selectedBookmark.title && (
+                      <p className='bookmark-detail'>
+                        <p className='red-details'>Title</p>{' '}
+                        {selectedBookmark.title}
+                      </p>
+                    )}
+                    {selectedBookmark.shortDescription && (
+                      <p className='bookmark-detail'>
+                        <p className='red-details'>Description</p>{' '}
+                        {selectedBookmark.shortDescription}
+                      </p>
+                    )}
+                    {selectedBookmark.url && (
+                      <a
+                        href={selectedBookmark.url}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='bookmark-detail'
+                      >
+                        <p className='red-details'>URL</p>{' '}
+                        {selectedBookmark.url}
+                      </a>
+                    )}
+                  </div>
+                  <div className='bookmark-display-button-container'>
+                    <button
+                      onClick={() => deleteFunc(selectedBookmark._id)}
+                      type='button'
+                      className='btn btn-danger'
+                    >
+                      Delete
+                    </button>
+                    <button type='button' className='btn btn-dark'>
+                      Edit
+                    </button>
+                  </div>
+                </div>
+              );
+            }
+          }}
+        </Consumer>
+      </div>
+    );
   }
 }
